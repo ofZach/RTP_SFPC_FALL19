5331906b2298e6d2ac92762729566c31123550a0
Lia Coleman <liailiad@gmail.com>
2019-11-13 21:31:18 -0500

    add cv comments

--- opencvExample3/src/ofApp.cpp
+++ opencvExample3/src/ofApp.cpp
@@ -1,18 +1,31 @@
 #include "ofApp.h"
 
+// Goal:
+// Draw the past 100 outlines of a human body moving, from a demo video.
+//
+
 //--------------------------------------------------------------
 void ofApp::setup(){
 
+    // Load the input sample video
     player.load("input.mov");
     player.play();
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    
+    // Advance the video player
     player.update();
+    
+    // If it is a new frame
     if (player.isFrameNew()){
-        finder.setMinArea(10);
-        finder.setThreshold(127);
+        
+        // Set some settings for the contour finder
+        finder.setMinArea(10); // We only keep track of contours that are big enough.
+        finder.setThreshold(127); // Use 127 as the threshold for finding contours. 127 is halfway between white and black
+        
+        // Find the contours in the current video frame
         finder.findContours(player);
     }
 }
@@ -22,18 +35,24 @@ void ofApp::draw(){
 
     ofBackground(0);
     
-    
+    // Draw the video player
     player.draw(0,0);
     //finder.draw();
     
     ofTranslate(player.getWidth(), 0);
     ofScale(2,2);
     
+    // If we have found a contour
     if (finder.size() > 0){
+        
+        // Get the first contour
         ofPolyline line = finder.getPolyline(0);
+        
+        // Make line pretty. Smooth it.
         line = line.getResampledBySpacing(1);
         line = line.getSmoothed(3);
         
+        // Keep track of a trail of the past 100 contours
         lines.push_back(line);
         if (lines.size() > 100){
             lines.erase(lines.begin());
@@ -42,6 +61,7 @@ void ofApp::draw(){
         
     }
     
+    // Draw the past 100 contours detected.
     for (int i = 0; i < lines.size(); i++){
         lines[i].draw();
     }
@@ -99,6 +119,6 @@ void ofApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo){
 
 }
